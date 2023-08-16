d8878465f2c22eec57679899fc5e1e61de38cc86
Nora Van Vleet <noravanvleet@gmail.com>
2023-08-16 10:41:49 -0400

    added router and confirmation page, still need to add configuration

--- index.js
+++ index.js
@@ -2,10 +2,17 @@ import express from 'express';
 import path from 'path';
 
 const app = express();
+const formRouter = require('./route/form');
 const port = 3000;
 
+// This deploys the application onto the local host at a given port
 app.listen(port, () => {
     console.log('Form web application is on http://localhost:' + port);
 });
 
-app.use(express.static(process.cwd()));
+// This serves the public directory
+app.use(express.static(path.join(process.cwd(), 'public')));
+
+// Enables the router to be used
+app.use('/form', formRouter);
+
