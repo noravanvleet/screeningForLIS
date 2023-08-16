ba6c9e625b824f96393005a65eab699df9994650
Nora Van Vleet <noravanvleet@gmail.com>
2023-08-16 11:53:41 -0400

    trying to fix localhosting issue

--- route/form.js
+++ route/form.js
@@ -4,16 +4,16 @@ import express from 'express';
 const router = express.Router();
 const connection = mysql.createConnection(config);
 
-// router.post('/formEntry', (res,req)=> {
-//     const name = req.body.name;
-//     const age = req.body.age;
-//     const title = req.body.title;
-//     const hometown = req.body.hometown;
+router.post('/formEntry', (res,req)=> {
+    const name = req.body.name;
+    const age = req.body.age;
+    const title = req.body.title;
+    const hometown = req.body.hometown;
 
-//     // connection.query(`INSERT INTO form(name, age, title, hometown) 
-//     //                   VALUES('${name}', '${age}'), '${title}', '${hometown}')`
-//     //                 );
+    connection.query(`INSERT INTO form(name, age, title, hometown) 
+                      VALUES('${name}', '${age}'), '${title}', '${hometown}')`
+                    );
 
-// });
+});
 
 export default router;
